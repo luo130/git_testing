aa20fdf4f36b2cc1a015194bf727766eb2cb64fb
lks <luokaisong@focaltech-electronics.com>
2023-10-31 15:53:23 +0800

    新增：新增函数add

--- test.c
+++ test.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+int add(int x, int y);
 
 int main()
 {
     printf("hello world\n");
+    printf("sum is: %d\n", add(100, 20));
     return 0;
+}
+
+
+int add(int x, int y)
+{
+    return (x) + (y);
 }
